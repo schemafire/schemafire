26f132a24384c7a7459d1b51de50ccb9eafac179
Ifiok Jr <ifiokotung@gmail.com>
2019-01-30 16:07:06 +0000

    refactor: index files in root of modules added no value

--- @skeema/firestore/index.ts
+++
@@ -1,4 +0,0 @@
-/* This file is used in development to resolve types for the package (because they live
-in `src/`) *without* needing to use the `types` `package.json` field. */
-
-export * from './src';
